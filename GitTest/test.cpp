d937f0097cc8975c1c08a3bc52a97e8e412a77f1
LeeSeongHa <toywwy@gmail.com>
2016-05-15 21:43:02 +0900

    커밋!

--- GitTest/test.cpp
+++ GitTest/test.cpp
@@ -6,13 +6,13 @@ using namespace std;
 int main(void)
 {
 	for (int i = 0; i < 10; i++) {
-		printf("Hello World\n");
+	printf("Hello World\n");
 	}
 
 	printf("master \n");
-	printf("master \n");
-
-
+	printf("Hello World\n");
+	printf("Hello World\n");
+	printf("Hello World\n");
 
 	return 0;
 }
