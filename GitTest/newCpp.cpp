e3febf24617be39267c684a19021fc0c3398334d
LeeSeongHa <toywwy@gmail.com>
2016-07-24 22:41:56 +0900

    for 문

--- GitTest/newCpp.cpp
+++ GitTest/newCpp.cpp
@@ -8,6 +8,14 @@ int main(void)
 	cout<<"Hello!!!"<<endl;
 
 	cout << "2010720111 !" << endl;
+	//Commit을 하는게 아니라 Push를 하는건가??
+
+
+	for (int i = 0; i < 10; i++)
+	{
+		cout << "이성하" << endl;
+	}
+
 
 	return 0;
 }
